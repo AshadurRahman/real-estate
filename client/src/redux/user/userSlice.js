6e0770565aa13d1959813b81dea8a4c65be7c871
Md Ashadur Rahman <md.ashadur.rahman@accenture.com>
2024-09-18 21:21:45 +0200

    SignOut functionality added

--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -53,6 +53,21 @@ const userSlice = createSlice({
       state.error = action.payload;
       state.loading = false;
     },
+
+    signOutUserStart: (state) => {
+      state.loading = true;
+    },
+
+    signOutUserSuccess: (state, action) => {
+      state.currentUser = null;
+      state.loading = false;
+      state.error = null;
+    },
+
+    signOutUserFailure: (state, action) => {
+      state.error = action.payload;
+      state.loading = false;
+    },
   },
 });
 
@@ -66,6 +81,9 @@ export const {
   deleteUserStart,
   deleteUserSuccess,
   deleteUserFailure,
+  signOutUserStart,
+  signOutUserSuccess,
+  signOutUserFailure,
 } = userSlice.actions;
 
 export default userSlice.reducer;
